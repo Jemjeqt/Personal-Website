bee5af9e24710c0357417802d8aba71c5061c7b4
jemje <azharkhairu29@gmail.com>
2024-11-15 22:40:04 +0700

    Update article
    new article

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="w-full md:w-3/5 h-20 mx-auto md:mt-5">
-        <div class="bg-white rounded-xl mx-3 p-5 md:p-10 md:mx-0">
-            <div>
+    <div class="w-full md:w-3/5 h-auto mx-auto md:mt-5">
+        <div class="bg-white rounded-xl mx-3 p-5 md:p-10 md:mx-0 relative">
+            <div class="md:mt-20">
                 <h1 class="text-xl md:text-4xl text-black text-left font-bold leading-relaxed">{{ title }}</h1>
                 <div class="mt-3 text-left text-gray-800 text-sm">Published at <span>{{ date }}</span></div>
                 <div class="h-[2px] w-20 my-5 md:my-10 bg-[#ffdb70] md:w-1/3 aos-init aos-animate mr-2"></div>
@@ -16,7 +16,6 @@
         </div>
     </div>
 </template>
-
 <script>
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
